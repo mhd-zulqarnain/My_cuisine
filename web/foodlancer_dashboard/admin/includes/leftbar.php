ade09999f8adefecb9e102195c95c3558a5d666b
Amna <aqsarafiq994@gmail.com>
2018-10-26 09:39:21 -0700

    26_october

--- web/foodlancer_dashboard/admin/includes/leftbar.php
+++ web/foodlancer_dashboard/admin/includes/leftbar.php
@@ -57,9 +57,13 @@
 										  </ul>
 </li> -->
 
-<li><a href="#"><i class="fa fa-sitemap"></i> Menu</a>
+<li><a href="#"><i class="fa fa-sitemap"></i> Post Menu</a>
 					<ul>
-						<li><a href="post-menu.php">Post Menu</a></li>
+						<li><a href="post-dailymenu.php">Daily Menu</a></li>
+						<li><a href="post-monthlymenu.php">Monthly Menu</a></li>
+						<li><a href="post-partymenu.php">Party Menu</a></li>
+						<li><a href="post-dietmenu.php">Diet Menu</a></li>
+						<li><a href="post-weektrialmenu.php">Weekly Trial</a></li>
 						<li><a href="manage-vehicles.php">Manage Menu</a></li>
 					</ul>
 				</li>
@@ -69,7 +73,6 @@
 			<li><a href="update-contactinfo.php"><i class="fa fa-files-o"></i> Update Contact Info</a></li>
 
 			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Manage Subscribers</a></li>
-			<li><a href="manage-subscribers.php"><i class="fa fa-table"></i> Calorie chart</a></li>
 
 			</ul>
 		</nav>
